479f9218953951dea73d89a5699c3bc565d1bb19
Roy <ekeneroy@gmail.com>
2023-02-15 19:00:53 -0600

    word-search pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) {
+        return true;
+      }
+      for (col of horizontalJoin[0]) {
+        if (col.includes(word))
+          return true;
+      }
     }
-}
+    
+    return false;
+  }
 
 module.exports = wordSearch
