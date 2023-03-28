079a4bbd11af6b651629db8879d8839955ea39e0
Roy <ekeneroy@gmail.com>
2023-03-27 19:24:52 -0500

    word search update

--- wordsearch.js
+++ wordsearch.js
@@ -1,16 +1,15 @@
 const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-      if (l.includes(word)) {
-        return true;
-      }
-      for (col of horizontalJoin[0]) {
-        if (col.includes(word))
-          return true;
-      }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-    
-    return false;
+    for (col of horizontalJoin[0]) {
+      if (col.includes(word)) return true;
+    }
   }
 
-module.exports = wordSearch
+  return false;
+};
+
+module.exports = wordSearch;
